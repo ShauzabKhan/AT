63babce88d0b19ce8a0c68dacc1034e835ebd6b6
Akiff29 <akiff.mi@processthreesixty.com>
2020-06-19 16:47:03 +0530

    Created a new folder in "Test_Upload" and kept one JPG file in it.

--- PegaTA/src/test/java/stepdefs/UploadDocument.java
+++ PegaTA/src/test/java/stepdefs/UploadDocument.java
@@ -1,11 +1,21 @@
 package stepdefs;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import com.google.inject.Inject;
+import com.p360.ta.utils.UIActions;
 import com.pega.TestEnvironment;
 import com.pega.framework.PegaWebDriver;
+import com.pega.framework.PegaWebElement;
 import com.pega.test.pega_sample_testframework.MyAppBrowser;
 import com.pega.test.pega_sample_testframework.MyAppTestEnvironment;
 
@@ -25,7 +35,7 @@ public class UploadDocument {
 }
 	
 	@Given("User attach document")
-	public void user_attach_document() {
+	public void user_attach_document() throws IOException {
 		
 String ExpandProject = "//h2[text()='Project documents']";
 		
@@ -36,7 +46,24 @@ String ExpandProject = "//h2[text()='Project documents']";
 		
 		pegaDriver.findElement(By.xpath(NavigatetoAttachSection)).click();
 		
-		pegaDriver.findElement(By.xpath("//input[@id='$PpyAttachmentPage$ppxAttachName']")).sendKeys("C:\\Users\\User\\Desktop\\111.JPG");
+		/*
+		 * FileInputStream file = new FileInputStream
+		 * ("C:\\Users\\User\\git\\AT1\\PegaTA\\data\\global-settings.properties");
+		 * 
+		 * Properties prop = new Properties ();
+		 * 
+		 * prop.load(file);
+		 * 
+		 * String path = prop.getProperty("path")+"C:\\Test_Upload\\111.JPG";
+		 */
+		
+		
+		  String currentDir = System.getProperty("user.dir");
+		  System.out.println("Current dir using System:" +currentDir);
+		 
+
+		
+		pegaDriver.findElement(By.xpath("//input[@id='$PpyAttachmentPage$ppxAttachName']")).sendKeys(currentDir+"\\Test_Upload\\111.JPG");
 		
 		//*[@id="$PpyAttachmentPage$ppxAttachName"]
 		
@@ -56,9 +83,14 @@ String Version = "//input[@id='9facf4ef']";
 		
 		String AttachmentsubmitButton = "//button[text()='  Attach ']";
 		
-		pegaDriver.findElement(By.xpath(AttachmentsubmitButton)).click();
+	//	pegaDriver.findElement(By.xpath(AttachmentsubmitButton)).click();
 		
+PegaWebElement Element = pegaDriver.findElement(By.xpath(AttachmentsubmitButton));
 		
+Element.click();
+		
+		Assert.assertNotNull(Element);
+				
 		
 	}
 }
--- PegaTA/src/test/resources/features/submitApplication.feature
+++ PegaTA/src/test/resources/features/submitApplication.feature
@@ -1,15 +1,34 @@
 Feature: Creates and submit application
 
-  #author : shauzab.khan
+  author : shauzab.khan
   @TC-02 @smoke @regression
-  Scenario: Create case scenario
-    Given A User logs in with "research.user14@hra.com" and "hrarules"
-    And User creates a case
-#    And User opens an assignment
+ Scenario: Create case scenario
+  Given A User logs in with "research.user14@hra.com" and "hrarules"
+  And User creates a case
+
     And User fills out the details
     And User submits
-    And fill the study info details
+   And fill the study info details
     And User attach document
    
-#      Then Successful submission message is displayed
+#   Then Successful submission message is displayed
  #  Then User logs off from portal
+ 
+ 
+ # @TC-03 @smoke @regression 
+#Scenario: pickup the workitem scenario
+ 
+#Given A User logs in with "research.user14@hra.com" and "hrarules"
+#And  User click on my task 
+
+#When User picks assignment with pyID
+#Then user click on update project details
+#And User fills out the details
+#And User submits
+#And fill the study info details
+#And User attach document
+ 
+ 
+ 
+ 
+ 
